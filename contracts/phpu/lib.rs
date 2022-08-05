4610e627871aa17b4ab8f2a11f1d3736edb07984
hgminerva <hgminerva@gmail.com>
2022-08-05 08:18:55 +0800

    PHPU Metadata

--- contracts/phpu/lib.rs
+++ contracts/phpu/lib.rs
@@ -5,10 +5,12 @@
 pub mod phpu {
     // imports from ink!
 	use ink_storage::traits::SpreadAllocate;
+    use ink_prelude::string::String;
     
     // imports from openbrush
 	use openbrush::contracts::psp22::extensions::burnable::*;
 	use openbrush::contracts::psp22::extensions::mintable::*;
+    use openbrush::contracts::psp22::extensions::metadata::*;
 	use openbrush::traits::Storage;
     
     #[ink(storage)]
@@ -16,6 +18,8 @@ pub mod phpu {
     pub struct Contract {
     	#[storage_field]
 		psp22: psp22::Data,
+        #[storage_field]
+        metadata: metadata::Data,
     }
     
     // Section contains default implementation without any modifications
@@ -25,8 +29,11 @@ pub mod phpu {
     
     impl Contract {
         #[ink(constructor)]
-        pub fn new(initial_supply: Balance) -> Self {
+        pub fn new(initial_supply: Balance, name: Option<String>, symbol: Option<String>, decimal: u8) -> Self {
             ink_lang::codegen::initialize_contract(|_instance: &mut Contract|{
+                _instance.metadata.name = name;
+                _instance.metadata.symbol = symbol;
+                _instance.metadata.decimals = decimal;
 				_instance._mint(_instance.env().caller(), initial_supply).expect("Should mint"); 
 			})
         }
